bd35fbe204b405afa350e391afbcf4e2d83a777d
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2022-08-17 10:51:37 +0600

    task item info

---
+++ src/Components/Item_Info/ItemInfo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ItemInfo = () => {
+   
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Item Information</h1>
+
+            
+
+        </div>
+    )
+}
+
+export default ItemInfo
